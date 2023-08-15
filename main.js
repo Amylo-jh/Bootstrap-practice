a787dcb8ef3c6c87918f793048b4a7fa3fc84238
Amylo <amylose1110@gmail.com>
2023-08-15 18:44:37 +0900

    10강 성능 최적화 수강 완료

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const emailInputEl = document.querySelector('#exampleInputEmail1')
-const modalEl = document.querySelector('#exampleModal')
+import Dropdown from 'bootstrap/js/dist/dropdown'
+import Modal from 'bootstrap/js/dist/modal'
 
-modalEl.addEventListener('shown.bs.modal', () => {
-  emailInputEl.focus()  
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
+
+const myModal = new Modal(document.getElementById('myModal'), {
+    backdrop: 'static'
 })
